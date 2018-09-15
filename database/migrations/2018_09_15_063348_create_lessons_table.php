fba96b826af9450b55017ac1df34308ad1cd7779
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-09-15 12:39:11 +0600

    Lesson model and migration

---
+++ database/migrations/2018_09_15_063348_create_lessons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('series_id')->nullable();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('episode_number');
+            $table->string('video_id');
+            $table->boolean('premium')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
